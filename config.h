9e10bd9030731ba74865d0b87437c3673fa66db6
akinhet <piotrzieniewicz2@gmail.com>
2025-02-10 18:45:27 +0100

    Changed color and formatting in config.h

--- config.h
+++ config.h
@@ -1,24 +1,24 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int gappx     = 10;       /* gap pixel between windows */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
-static const unsigned int systrayonleft = 0;    /* 0: systray in the right corner, >0: systray on left of status text */
-static const unsigned int systrayspacing = 2;   /* systray spacing */
-static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
-static const int showsystray        = 1;        /* 0 means no systray */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Source Code Pro:size=10" };
-static const char dmenufont[]       = "Source Code Pro:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
-static const char *colors[][3]      = {
+static const unsigned int borderpx       = 1;  /* border pixel of windows */
+static const unsigned int gappx          = 10; /* gap pixel between windows */
+static const unsigned int snap           = 32; /* snap pixel */
+static const unsigned int systraypinning = 0;  /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayonleft  = 0;  /* 0: systray in the right corner, >0: systray on left of status text */
+static const unsigned int systrayspacing = 2;  /* systray spacing */
+static const int systraypinningfailfirst = 1;  /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray             = 1;  /* 0 means no systray */
+static const int showbar                 = 1;  /* 0 means no bar */
+static const int topbar                  = 1;  /* 0 means bottom bar */
+static const char *fonts[]               = { "SauceCodePro Nerd Font:size=10" };
+static const char dmenufont[]            = "SauceCodePro Nerd Font:size=10";
+static const char col_gray1[]            = "#222222";
+static const char col_gray2[]            = "#444444";
+static const char col_gray3[]            = "#bbbbbb";
+static const char col_gray4[]            = "#eeeeee";
+static const char col_cyan[]             = "#a00000";
+static const char *colors[][3] = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_gray1, col_cyan  },
@@ -27,30 +27,30 @@ static const char *colors[][3]      = {
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
-static const unsigned int ulinepad	= 4;	/* horizontal padding between the underline and tag */
-static const unsigned int ulinestroke	= 2;	/* thickness / height of the underline */
-static const unsigned int ulinevoffset	= 0;	/* how far above the bottom of the bar the line should appear */
-static const int ulineall 		= 0;	/* 1 to show underline on all tags, 0 for just the active ones */
+static const unsigned int ulinepad      = 4; /* horizontal padding between the underline and tag */
+static const unsigned int ulinestroke   = 2; /* thickness / height of the underline */
+static const unsigned int ulinevoffset  = 0; /* how far above the bottom of the bar the line should appear */
+static const int ulineall               = 0; /* 1 to show underline on all tags, 0 for just the active ones */
 
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "zen-alpha", NULL,             NULL,           1 << 0,       0,            0 },
-	{ "feishin",   NULL,             NULL,           1 << 8,       0,            0 },
-	{ "vesktop",   NULL,             NULL,           1 << 8,       0,            1 },
-	{ "steam_app", "steam_app",      NULL,           1 << 6,       0,            1 },
-	{ "steam",     "steamwebhelper", NULL,           1 << 7,       0,            1 },
-	{ "steam",     "steamwebhelper", "Friends List", 1 << 7,       1,            1 },
+	/* class       instance          title           tags mask  isfloating  monitor */
+	{ "zen",       NULL,             NULL,           1 << 0, 0, 0 },
+	{ "feishin",   NULL,             NULL,           1 << 8, 0, 0 },
+	{ "vesktop",   NULL,             NULL,           1 << 8, 0, 1 },
+	{ "steam_app", "steam_app",      NULL,           1 << 6, 0, 1 },
+	{ "steam",     "steamwebhelper", NULL,           1 << 7, 0, 1 },
+	{ "steam",     "steamwebhelper", "Friends List", 1 << 7, 1, 1 },
 };
 
 /* layout(s) */
-static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
-static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
-static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
+static const float mfact        = 0.55; /* factor of master area size [0.05..0.95] */
+static const int nmaster        = 1;    /* number of clients in master area */
+static const int resizehints    = 1;    /* 1 means respect size hints in tiled resizals */
+static const int lockfullscreen = 1;    /* 1 will force focus on the fullscreen window */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
@@ -72,11 +72,12 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] 		= { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  		= { "alacritty", NULL };
-static const char *screenshotcmd[]	= { "/bin/sh", "-c", "maim -s Pictures/Screenshots/$(date +%s).png | xclip -selection clipboard -t image/png", NULL };
-static const char *wallpapercmd[] 	= { "/home/akinhet/git/small-scripts/wallpaper.sh", NULL };
-static const char *powercmd[] 		= { "/home/akinhet/git/small-scripts/powermenu.sh", NULL };
+
+static const char *dmenucmd[]      = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *termcmd[]       = { "alacritty", NULL };
+static const char *screenshotcmd[] = { "/bin/sh", "-c", "maim -s | tee /home/akinhet/Pictures/Screenshots/$(date +%s).png | xclip -selection clipboard -t image/png", NULL };
+static const char *wallpapercmd[]  = { "/home/akinhet/git/small-scripts/wallpaper.sh", NULL };
+static const char *powercmd[]      = { "/home/akinhet/git/small-scripts/powermenu.sh", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -118,8 +119,8 @@ static const Key keys[] = {
 	{ MODKEY|ControlMask|ShiftMask, XK_q,      quit,           {1} },
 	// custom
 	{ MODKEY|ShiftMask,             XK_Pause,  spawn,          {.v = powercmd } },
-	{ ShiftMask,             	XK_Pause,  spawn,          {.v = screenshotcmd } },
-	{ MODKEY|ShiftMask,            	XK_r,	   spawn,          {.v = wallpapercmd } },
+	{ ShiftMask,                    XK_Pause,  spawn,          {.v = screenshotcmd } },
+	{ MODKEY|ShiftMask,             XK_r,      spawn,          {.v = wallpapercmd } },
 };
 
 /* button definitions */
